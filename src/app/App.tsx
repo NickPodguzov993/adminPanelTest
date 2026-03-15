37b65f180b55a2eb226225ed4cfbf78df35cd4ff
nick <user@MacBook-Pro.local>
2026-03-15 19:24:29 +0300

    fix deploy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ import { router } from "./router";
 export default function App() {
     return (
         <>
-            <div style={{ padding: 8, fontSize: 12 }}>DEPLOY TEST v2</div>
+           
             <RouterProvider router={router} />
             <Toaster position="top-right" />
         </>
