c6445a8d6b10261ab8b4ebba7daf46db3a262397
nick <user@MacBook-Pro.local>
2026-03-15 15:49:05 +0300

    fix router basename for github pages

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -3,22 +3,27 @@ import { LoginPage } from "../../pages/login/LoginPage";
 import { ProductsPage } from "../../pages/products/ProductsPage";
 import { ProtectedRoute } from "../../features/auth/ui/ProtectedRoute";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+    [
+        {
+            path: "/login",
+            element: <LoginPage />,
+        },
+        {
+            element: <ProtectedRoute />,
+            children: [
+                {
+                    path: "/",
+                    element: <ProductsPage />,
+                },
+                {
+                    path: "/products",
+                    element: <ProductsPage />,
+                },
+            ],
+        },
+    ],
     {
-        path: "/login",
-        element: <LoginPage />,
-    },
-    {
-        element: <ProtectedRoute />,
-        children: [
-            {
-                path: "/",
-                element: <ProductsPage />,
-            },
-            {
-                path: "/products",
-                element: <ProductsPage />,
-            },
-        ],
-    },
-]);
+        basename: "/adminPanelTest",
+    }
+);
